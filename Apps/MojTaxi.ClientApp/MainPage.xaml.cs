6b78fd5617feb44af611424c3a7046cfcc38ab8d
edin-beg <edin.begovic@it-craft.ba>
2025-11-11 12:20:06 +0100

    Add location permissions and integrate map functionality; update color scheme

--- Apps/MojTaxi.ClientApp/MainPage.xaml.cs
+++ Apps/MojTaxi.ClientApp/MainPage.xaml.cs
@@ -1,52 +1,37 @@
-﻿namespace MojTaxi.ClientApp.Pages;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MojTaxi.ClientApp.ViewModels;
+
+namespace MojTaxi.ClientApp.Pages;
+using Microsoft.Maui.Maps;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
-        SetActiveTab(BtnProfile); // default
+        SetUserLocation();
+    }
+
+    private async void SetUserLocation()
+    {
+        var location = await Geolocation.GetLastKnownLocationAsync();
+        if (location != null)
+        {
+            mapControl.MoveToRegion(MapSpan.FromCenterAndRadius(
+                new Location(location.Latitude, location.Longitude),
+                Distance.FromKilometers(1)
+            ));
+        }
     }
 
     private void OnTabClicked(object sender, EventArgs e)
     {
-        var button = (Button)sender;
-        SetActiveTab(button);
-
-        string tab = button.CommandParameter.ToString();
-
-        switch (tab)
-        {
-            case "Profile":
-                DisplayAlert("TAB", "Profil otvoren", "OK");
-                break;
-            case "Pets":
-                DisplayAlert("TAB", "Ljubimci otvoreni", "OK");
-                break;
-            case "Kids":
-                DisplayAlert("TAB", "Djeca otvorena", "OK");
-                break;
-            case "Special":
-                DisplayAlert("TAB", "Posebno otvoreno", "OK");
-                break;
-        }
-    }
-
-    private void SetActiveTab(Button active)
-    {
-        // reset svih buttona
-        BtnProfile.TextColor = Colors.Gray;
-        BtnPets.TextColor = Colors.Gray;
-        BtnKids.TextColor = Colors.Gray;
-        BtnSpecial.TextColor = Colors.Gray;
-
-        BtnProfile.BackgroundColor = Color.FromHex("#222222");
-        BtnPets.BackgroundColor = Color.FromHex("#222222");
-        BtnKids.BackgroundColor = Color.FromHex("#222222");
-        BtnSpecial.BackgroundColor = Color.FromHex("#222222");
-
-        // aktivni tab
-        active.TextColor = Colors.Black;
-        active.BackgroundColor = Colors.Yellow;
+        var btn = (Button)sender;
+        string param = btn.CommandParameter?.ToString();
+        // handle tab click...
     }
 }
--- Apps/MojTaxi.ClientApp/ViewModels/RegistrationViewModel.cs
+++ Apps/MojTaxi.ClientApp/ViewModels/RegistrationViewModel.cs
@@ -1,46 +1,44 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MojTaxi.Core.Abstractions;
+using System.Collections.ObjectModel;
 
 namespace MojTaxi.ClientApp.ViewModels;
 
-using System.Collections.ObjectModel;
-using System.Windows.Input;
-using Models;
-
-
-public class RegistrationViewModel : ObservableObject
+public partial class RegistrationViewModel : ObservableObject
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string PhoneNumber { get; set; } // set from previous screen
-    public string Email { get; set; }
-    public ImageSource ProfileImage { get; set; }
+    [ObservableProperty]
+    private string firstName;
 
-    public ICommand PickImageCommand { get; }
-    public ICommand SubmitRegistrationCommand { get; }
+    [ObservableProperty]
+    private string lastName;
 
-    public RegistrationViewModel()
-    {
-        PickImageCommand = new Command(async () => await PickImage());
-        SubmitRegistrationCommand = new Command(async () => await Submit());
-    }
+    [ObservableProperty]
+    private string phoneNumber;
 
+    [ObservableProperty]
+    private string email;
+
+    [ObservableProperty]
+    private ImageSource profileImage;
+
+    public RegistrationViewModel() { }
+
+    [RelayCommand]
     private async Task PickImage()
     {
         FileResult photo = await MediaPicker.PickPhotoAsync();
         if (photo != null)
+        {
             ProfileImage = ImageSource.FromFile(photo.FullPath);
-
-        OnPropertyChanged(nameof(ProfileImage));
+        }
     }
 
+    [RelayCommand]
     private async Task Submit()
     {
-        // Validation + API poziv za kreiranje usera
-        await App.Current.MainPage.DisplayAlert("OK", "Registracija uspješna", "OK");
+        // TODO: Validacija + API poziv
 
-        await Shell.Current.GoToAsync("//MainPage");
-
+        await Shell.Current.GoToAsync("MainPage");
     }
 }
